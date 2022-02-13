580a4cb260e9a4b6c78a5b7c763364df225a3f7d
EuclidesXcode <42277822+EuclidesXcode@users.noreply.github.com>
2022-02-13 00:17:37 -0300

    [FEATURE] New configuration on serverless

--- handler.js
+++ handler.js
@@ -1,18 +1,68 @@
-'use strict';
+const AWS = require("aws-sdk");
+const express = require("express");
+const serverless = require("serverless-http");
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+const app = express();
+
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+app.use(express.json());
+
+app.get("/users/:userId", async function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  try {
+    const { Item } = await dynamoDbClient.get(params).promise();
+    if (Item) {
+      const { userId, name } = Item;
+      res.json({ userId, name });
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive user" });
+  }
+});
+
+app.post("/users", async function (req, res) {
+  const { userId, name } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== "string") {
+    res.status(400).json({ error: '"name" must be a string' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    res.json({ userId, name });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create user" });
+  }
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+
+module.exports.handler = serverless(app);
